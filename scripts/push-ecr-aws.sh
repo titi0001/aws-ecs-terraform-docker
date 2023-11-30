a987539723b98e97f7a37bc0dd8a4e7018ff7347
Thiago <titiura@gmail.com>
2023-11-30 08:52:20 -0300

    feat: add tag do scrpit verify image

--- scripts/push-ecr-aws.sh
+++ scripts/push-ecr-aws.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 # Executa o comando docker images e extrai apenas os nomes dos repositórios
-nomes_dos_repositorios=$(docker images --format "{{.Repository}}")
+nomes_dos_repositorios=$(docker images --format "{{.Repository}}:{{.Tag}}")
 
 # Define a parte numérica inicial com até 12 números
 numeros_iniciais="[0-9]{1,12}"
+tag="[a-z0-9-]{1,50}"
 
 # Define o formato do nome da imagem que deseja verificar
-formato_da_imagem="${numeros_iniciais}.dkr.ecr.us-west-2.amazonaws.com/[a-zA-Z]+"
+formato_da_imagem="${numeros_iniciais}\.dkr\.ecr\.us-west-2\.amazonaws\.com/[a-zA-Z]+:${tag}"
 
 # Cria um regex que corresponda ao formato da imagem
-regex="^${numeros_iniciais}\.dkr\.ecr\.us-west-2\.amazonaws\.com/[a-zA-Z]+$"
+regex="^${numeros_iniciais}\.dkr\.ecr\.us-west-2\.amazonaws\.com/[a-zA-Z]+:${tag}$"
 
 # Flag para indicar se uma imagem correspondente foi encontrada
 imagem_correspondente_encontrada=false
@@ -22,12 +23,14 @@ while read -r nome_do_repositorio; do
         imagem_correspondente_encontrada=true
         echo "Imagem correspondente encontrada: $nome_do_repositorio"
         # Adicione aqui qualquer ação adicional que deseja executar para uma imagem correspondente
+        # Faça o push da imagem para o Amazon ECR
+        docker push ${nome_do_repositorio}
+        # Exiba mensagem de conclusão
+        echo "Imagem Docker foi enviada para o Amazon ECR com sucesso!"
     fi
 done <<< "$nomes_dos_repositorios"
 
 # Verifica se pelo menos uma imagem correspondente foi encontrada
-if [ "$imagem_correspondente_encontrada" = true ]; then
-    echo "Pelo menos uma imagem correspondente foi encontrada."
-else
+if [ "$imagem_correspondente_encontrada" = false ]; then
     echo "Nenhuma imagem correspondente foi encontrada."
 fi
