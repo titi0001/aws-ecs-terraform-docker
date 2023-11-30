9917bc2e3023f412c8867a01251764d17fa5abf4
Thiago <titiura@gmail.com>
2023-11-30 10:29:58 -0300

    feat: create secGroup

---
+++ infra/securityGroup.tf
@@ -0,0 +1,22 @@
+resource "aws_security_group" "alb" {
+  name        = "alb-ECS"
+  vpc_id      = module.vpc.vpc_id
+}
+
+resource "aws_security_group_rule" "alb_tcp" {
+  type              = "ingress"
+  from_port         = 8000
+  to_port           = 8000
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.alb.id
+}
+
+resource "aws_security_group_rule" "alb_tcp" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.alb.id
+}
